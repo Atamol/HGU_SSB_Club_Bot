23de2eb3aa8126ab6935dee285b7cd4b271ab773
Atamol <35617831+Atamol@users.noreply.github.com>
2024-11-05 02:44:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from discord.ui import View
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("TOKEN")
 channel_1_id = int(os.getenv("CHANNEL_1_ID"))
 channel_2_id = int(os.getenv("CHANNEL_2_ID"))
 
